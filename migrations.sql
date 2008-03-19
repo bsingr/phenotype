cc0a7a3ce88933d94114dd3c36889a14436865f8
nils73 <nils73@3661cc67-7a12-0410-bb00-87ca1905b2a9>
2008-03-19 08:45:55 +0000

    aktuelle DB-Änderungen
    git-svn-id: https://phenotype.svn.sourceforge.net/svnroot/phenotype/trunk@238 3661cc67-7a12-0410-bb00-87ca1905b2a9

--- migrations.sql
+++ migrations.sql
@@ -34,12 +34,10 @@ ALTER TABLE `content_template` CHANGE `tpl_id` `tpl_id` INT( 11 ) NOT NULL AUTO_
 
 ALTER TABLE `extra_template` CHANGE `tpl_id` `tpl_id` INT( 11 ) NOT NULL AUTO_INCREMENT;
 
-
-
 ALTER TABLE `dataobject` ADD `dao_params` VARCHAR( 255 ) NOT NULL AFTER `dao_bez` ;
 ALTER TABLE `dataobject` ADD `dao_type` TINYINT NOT NULL ;
 ALTER TABLE `dataobject` DROP `dao_lastbuild_time`;
-ALTER TABLE `dataobject` DROP INDEX `dao_bez`  
+ALTER TABLE `dataobject` DROP INDEX `dao_bez`;  
 ALTER TABLE `dataobject` ADD `dao_clearonedit` TINYINT NOT NULL ;
 
 ALTER TABLE `page` ADD `pag_url1` VARCHAR( 255 ) NOT NULL AFTER `pag_url` ,
@@ -48,3 +46,7 @@ ADD `pag_url3` VARCHAR( 255 ) NOT NULL AFTER `pag_url2` ,
 ADD `pag_url4` VARCHAR( 255 ) NOT NULL AFTER `pag_url3` ;
 
 ALTER TABLE `pagegroup` ADD `grp_smarturl_schema` TINYINT NOT NULL DEFAULT '1';
+
+
+
+ALTER TABLE `page` ADD `pag_props` TEXT NOT NULL AFTER `pag_url4` ;
